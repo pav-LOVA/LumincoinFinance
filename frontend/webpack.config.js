e4f228dad090dc5bb77b3d47d92905f8dc7f2c9a
Ekaterina Pavlova <tanaeva743@gmail.com>
2025-07-28 21:11:32 +0300

    update first exercise

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -39,15 +39,7 @@ module.exports = {
                 {from: "./src/templates", to: "templates"},
                 { from: "./src/static/images", to: "images" },
 
-                // { from: "./node_modules/admin-lte/dist/css/adminlte.min.css", to: "css" },
                 { from: "./node_modules/jquery/dist/jquery.min.js", to: "js" },
-                // { from: "./node_modules/admin-lte/dist/js/adminlte.min.js", to: "js" },
-
-
-                // { from: "./node_modules/bootstrap/dist/css/bootstrap.min.css", to: "css" },
-                // { from: "./node_modules/bootstrap/dist/js/bootstrap.min.js", to: "js" },
-
-
                 { from: "./node_modules/icheck-bootstrap/icheck-bootstrap.min.css", to: "css" },
 
 
